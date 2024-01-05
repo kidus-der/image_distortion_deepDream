52f94853032d12a48ac72138b8525b3456f57612
kidus-der <101529458+kidus-der@users.noreply.github.com>
2024-01-05 16:42:40 -0700

    added some helper functions for graph visualisation

--- distort_image.py
+++ distort_image.py
@@ -1,10 +1,11 @@
 import os
 import zipfile
-import numpy as numpy
+import numpy as np
 import tensorflow as tf
 from functools import partial
 import PIL.Image
 import urllib.request
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -23,6 +24,7 @@ def main():
         with zipfile.ZipFile(loc_zip_file, 'r') as zip_refer:
             zip_refer.extractall(data_dir)
 
+    img_noise = np.random.uniform(size=(224, 224, 3)) + 100.0
     model_func = 'tensorflow_inception_graph.pb'
 
     # now we create a TF session and load the graph
@@ -31,7 +33,7 @@ def main():
     with tf.gfile.FastGFile(os.path.join(data_dir, model_func), 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-    tensor_input = tf.placeholder(numpy.float32, name='input') # our input tensor
+    tensor_input = tf.placeholder(np.float32, name='input') # our input tensor
     imagenetwork_mean = 117.0
     tensor_preprocessed = tf.expand_dims(tensor_input - imagenetwork_mean, 0)
     tf.import_graph_def(graph_def, {'input': tensor_preprocessed})
@@ -47,4 +49,28 @@ def main():
     print(f'Feature channels: {sum(feature_nums)}')
     print(f'Feature numbers: {feature_nums}')
 
-main()
+    # strip large constants from graph
+    def strip_constants(graph_def, max_const_size=32):
+        
+        strip_def = tf.GraphDef()
+        for n_0 in graph_def.node:
+            n = strip_def.node.add()
+            n.MergeFrom(n_0)
+            if n.op == 'Const':
+                tensor = n.attr['value'].tensor
+                size = len(tensor.tensor_content)
+                if size > max_const_size:
+                    tensor.tensor_content = "<stripped %d bytes>"%size
+        return strip_def
+    
+    #rename node identifiers
+    def rename_nodes(graph_def, rename_func):
+        
+        res_def = tf.GraphDef()
+        for n_0 in graph_def.node:
+            n = res_def.node.add()
+            n.MergeFrom(n_0)
+            n.name = rename_func(n.name)
+            for i, s in enumerate(n.input):
+                n.input[i] = rename_func(s) if s[0]!='^' else '^'+rename_func(s[1:])
+        return res_def
